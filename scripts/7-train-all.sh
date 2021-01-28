6871cc4105e574485442cbdc1a38054c69266b13
Alp <alp@collectivat.cat>
2021-01-28 16:25:29 +0100

    new training test

---
+++ scripts/7-train-all.sh
@@ -0,0 +1,34 @@
+TRAINID="A"
+BPEID="BPE-mymtedmix-6000"
+echo Training setup $TRAINID
+bash 7a-train-step1.sh $TRAINID BPE-mymtedmix-6000 swcmix 2048 1500
+echo 
+bash 7b-train-step2.sh $TRAINID BPE-mymtedmix-6000 twbmix 40 40
+echo 
+
+# TRAINID="B"
+# echo Training setup $TRAINID
+# bash 7a-train-step1.sh $TRAINID BPE-mymtedmix-6000 swmix 2048 5300
+# echo 
+# bash 7b-train-step2.sh $TRAINID BPE-mymtedmix-6000 swcmix 2048 1500
+# echo 
+# bash 7c-train-step3.sh $TRAINID BPE-mymtedmix-6000 twbmix 40 40
+# echo 
+
+# TRAINID="C"
+# echo Training setup $TRAINID
+# bash 7a-train-step1.sh $TRAINID BPE-mymtedmix-6000 mtedmix 2048 5300
+# echo 
+# bash 7b-train-step2.sh $TRAINID BPE-mymtedmix-6000 swcmix 2048 1500
+# echo 
+# bash 7c-train-step3.sh $TRAINID BPE-mymtedmix-6000 twbmix 40 40
+# echo 
+
+# TRAINID="D"
+# echo Training setup $TRAINID
+# bash 7a-train-step1.sh $TRAINID BPE-mymtedmix-6000 monomix 2048 5300
+# echo 
+# bash 7b-train-step2.sh $TRAINID BPE-mymtedmix-6000 swcmix 2048 1500
+# echo 
+# bash 7c-train-step3.sh $TRAINID BPE-mymtedmix-6000 twbmix 40 40
+# echo 
--- scripts/7b-train-step2.sh
+++ scripts/7b-train-step2.sh
@@ -9,50 +9,65 @@ MODELTYPE="inswc"
 
 #PROCEDURES
 function do_train() {
-	BASEMODELNAME=$MODELPREFIX-$MODELSRC-$MODELTGT-$BASEMODELTYPE-$BASEMODELID
-	MODELNAME=$MODELPREFIX-$MODELSRC-$MODELTGT-$MODELTYPE-$BASEMODELID-$MODELID
-	mkdir -p $MODELDIR/$MODELNAME
-	mkdir -p $LOGDIR
+    BASEMODELNAME=$MODELPREFIX-$BASEMODELSRC-$BASEMODELTGT-$BASEMODELTYPE-$BASEMODELID
+    MODELNAME=$MODELPREFIX-$MODELSRC-$MODELTGT-$MODELTYPE-$BASEMODELID-$MODELID
 
-	onmt_train -data $DATADIR/$DATASET.$BPEID -train_from $MODELDIR/$BASEMODELNAME/${BASEMODELNAME}_best.pt \
-	       	-save_model $MODELDIR/$MODELNAME/$MODELNAME \
-          	-layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2048 -heads 8  \
-     	  	-encoder_type transformer -decoder_type transformer -position_encoding \
-	  	-max_generator_batches 2 -dropout 0.2 \
-	  	-batch_size $BATCHSIZE -batch_type tokens -normalization tokens  -accum_count 2 \
-	  	-optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
-	  	-max_grad_norm 0 -param_init 0  -param_init_glorot -label_smoothing 0.1 \
-	  	-train_steps 1000000 -early_stopping 5 -early_stopping_criteria ppl \
-	  	-valid_steps $VALIDSAVE -save_checkpoint_steps $VALIDSAVE -report_every $REPORT \
-	  	-world_size 2 -gpu_ranks 0 1 2>&1 | tee $LOGDIR/train-$MODELNAME.log
-	
-	#Make a symlink to best model
-	BESTSTEP=`cat $LOGDIR/train-$MODELNAME.log | grep "Best model found at step" | rev | cut -d' ' -f1 | rev`
-	BESTMODEL=$MODELDIR/$MODELNAME/${MODELNAME}_step_${BESTSTEP}.pt
-	echo $BESTMODEL
-	ln -sf $BESTMODEL $MODELDIR/$MODELNAME/${MODELNAME}_best.pt
+    echo Training $MODELNAME from $BASEMODELNAME
+    echo using data $DATASET.$BPEID
+
+    mkdir -p $MODELDIR/$MODELNAME
+    mkdir -p $LOGDIR
+
+    onmt_train -data $DATADIR/$DATASET.$BPEID -train_from $MODELDIR/$BASEMODELNAME/${BASEMODELNAME}_best.pt \
+            -save_model $MODELDIR/$MODELNAME/$MODELNAME \
+            -layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2048 -heads 8  \
+            -encoder_type transformer -decoder_type transformer -position_encoding \
+        -max_generator_batches 2 -dropout 0.2 \
+        -batch_size $BATCHSIZE -batch_type tokens -normalization tokens  -accum_count 2 \
+        -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
+        -max_grad_norm 0 -param_init 0  -param_init_glorot -label_smoothing 0.1 \
+        -train_steps 1000000 -early_stopping 5 -early_stopping_criteria ppl \
+        -valid_steps $VALIDSAVE -save_checkpoint_steps $VALIDSAVE -report_every $REPORT \
+        -world_size 2 -gpu_ranks 0 1 2>&1 | tee $LOGDIR/train-$MODELNAME.log
+    
+    #Make a symlink to best model
+    BESTSTEP=`cat $LOGDIR/train-$MODELNAME.log | grep "Best model found at step" | rev | cut -d' ' -f1 | rev`
+    BESTMODEL=$MODELDIR/$MODELNAME/${MODELNAME}_step_${BESTSTEP}.pt
+    echo $BESTMODEL
+    ln -sf $BESTMODEL $MODELDIR/$MODELNAME/${MODELNAME}_best.pt
 }
 
+#PARAMETERS
+MODELPREFIX=$1
+BPEID=$2
+DATASETID=$3
+BATCHSIZE=$4
+VALIDSAVE=$5
+
 #CALLS
-MODELPREFIX="monomix"
-BPEID="BPE-monomix-6000"
-MODELSRC="fr"
-MODELTGT="sw"
+# MODELPREFIX="monomix"
+# BPEID="BPE-monomix-6000"
+BASEMODELSRC="fr"
+BASEMODELTGT="sw"
+MODELSRC="fra"
+MODELTGT="swc"
 BASEMODELTYPE="generic"
 BASEMODELID="s001"
 MODELID="i001"
-DATASET="swcmix.fra-swc"
-BATCHSIZE=2048
-VALIDSAVE=1500
+DATASET="$DATASETID.$MODELSRC-$MODELTGT"
+# BATCHSIZE=2048
+# VALIDSAVE=1500
 REPORT=50
 do_train
 
-MODELSRC="sw"
-MODELTGT="fr"
+BASEMODELSRC="sw"
+BASEMODELTGT="fr"
+MODELSRC="swc"
+MODELTGT="fra"
 BASEMODELTYPE="generic"
 BASEMODELID="s001"
 MODELID="i001"
-DATASET="swcmix.swc-fra"
+DATASET="$DATASETID.$MODELSRC-$MODELTGT"
 do_train
 
 #ending alert 
