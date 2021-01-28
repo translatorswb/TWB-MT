e41742a14e1dd9abca39fa4470f43dc68131ddff
Alp <alp@collectivat.cat>
2021-01-28 17:00:18 +0100

    uncomment

--- scripts/7b-train-step2.sh
+++ scripts/7b-train-step2.sh
@@ -19,23 +19,23 @@ function do_train() {
     mkdir -p $MODELDIR/$MODELNAME
     mkdir -p $LOGDIR
 
-    # onmt_train -data $DATADIR/$DATASET.$BPEID -train_from $MODELDIR/$BASEMODELNAME/${BASEMODELNAME}_best.pt \
-    #         -save_model $MODELDIR/$MODELNAME/$MODELNAME \
-    #         -layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2048 -heads 8  \
-    #         -encoder_type transformer -decoder_type transformer -position_encoding \
-    #     -max_generator_batches 2 -dropout 0.2 \
-    #     -batch_size $BATCHSIZE -batch_type tokens -normalization tokens  -accum_count 2 \
-    #     -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
-    #     -max_grad_norm 0 -param_init 0  -param_init_glorot -label_smoothing 0.1 \
-    #     -train_steps 1000000 -early_stopping 5 -early_stopping_criteria ppl \
-    #     -valid_steps $VALIDSAVE -save_checkpoint_steps $VALIDSAVE -report_every $REPORT \
-    #     -world_size 2 -gpu_ranks 0 1 2>&1 | tee $LOGDIR/train-$MODELNAME.log
+    onmt_train -data $DATADIR/$DATASET.$BPEID -train_from $MODELDIR/$BASEMODELNAME/${BASEMODELNAME}_best.pt \
+            -save_model $MODELDIR/$MODELNAME/$MODELNAME \
+            -layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2048 -heads 8  \
+            -encoder_type transformer -decoder_type transformer -position_encoding \
+        -max_generator_batches 2 -dropout 0.2 \
+        -batch_size $BATCHSIZE -batch_type tokens -normalization tokens  -accum_count 2 \
+        -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
+        -max_grad_norm 0 -param_init 0  -param_init_glorot -label_smoothing 0.1 \
+        -train_steps 1000000 -early_stopping 5 -early_stopping_criteria ppl \
+        -valid_steps $VALIDSAVE -save_checkpoint_steps $VALIDSAVE -report_every $REPORT \
+        -world_size 2 -gpu_ranks 0 1 2>&1 | tee $LOGDIR/train-$MODELNAME.log
     
-    # #Make a symlink to best model
-    # BESTSTEP=`cat $LOGDIR/train-$MODELNAME.log | grep "Best model found at step" | rev | cut -d' ' -f1 | rev`
-    # BESTMODEL=$MODELDIR/$MODELNAME/${MODELNAME}_step_${BESTSTEP}.pt
-    # echo $BESTMODEL
-    # ln -sf $BESTMODEL $MODELDIR/$MODELNAME/${MODELNAME}_best.pt
+    #Make a symlink to best model
+    BESTSTEP=`cat $LOGDIR/train-$MODELNAME.log | grep "Best model found at step" | rev | cut -d' ' -f1 | rev`
+    BESTMODEL=$MODELDIR/$MODELNAME/${MODELNAME}_step_${BESTSTEP}.pt
+    echo $BESTMODEL
+    ln -sf $BESTMODEL $MODELDIR/$MODELNAME/${MODELNAME}_best.pt
 }
 
 #PARAMETERS
