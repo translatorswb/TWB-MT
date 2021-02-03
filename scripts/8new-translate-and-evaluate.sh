d629e0d4f0bfa6db84c3fde8e3d1d979fff33445
Alp <alp@collectivat.cat>
2021-02-03 16:37:50 +0100

    merge translate and evaluate

---
+++ scripts/8new-translate-and-evaluate.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#INITIALIZATIONS (Do not edit)
+FS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CORPORADIR="$FS/../corpora"
+DATADIR="$FS/../onmt/data"
+MODELDIR="$FS/../onmt/models"
+TESTDIR="$FS/../onmt/test"
+EVALDIR="$FS/../onmt/eval"
+
+#PROCEDURES
+function do_test() {
+	mkdir -p $TESTDIR
+	mkdir -p $EVALDIR
+
+	MODELPATH=`ls $MODELDIR/$MODELNAME/${MODELNAME}_best_*`
+	BPE=$BPEDIR/$BPEID.codes
+	INFERENCEOUT=$C.$SRC.inf-$MODELNAME.$TGT
+	EVALOUT=$INFERENCEOUT.results.txt
+
+	echo bash $FS/translator.sh $CORPORADIR/$CORPUS/$C.$SRC $CORPORADIR/$CORPUS/$INFERENCEOUT $MODELPATH $BPE
+	
+	echo cat $CORPORADIR/$CORPUS/$INFERENCEOUT | sacrebleu $CORPORADIR/$CORPUS/$C.$TGT -tok none --metrics {bleu,chrf} | tee $EVALDIR/$EVALOUT
+}
+
+
+#CALLS
+BPEID="BPE-fixmix-6000"
+SRC="swc"
+TGT="fra"
+CORPUS="toy"
+C="toy"
+MODELNAME="TOY-fr-sw-generic-s001"
+do_test
+
+
