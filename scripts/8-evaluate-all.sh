1971b8db5c0245954f7f8ce88fd9045c7831d2fb
alpoktem <alp@collectivat.cat>
2021-02-05 14:31:58 +0200

    trainid, corpusid as variables

--- scripts/8-evaluate-all.sh
+++ scripts/8-evaluate-all.sh
@@ -1,21 +1,58 @@
+# Batch evaluation script 
+# Example call
+# $ bash 8-evaluate-all.sh D test.twbkit test-twbkit > ../results/D-twbkit.txt
+
 BPEID=BPE-fixmix-6000
 
-TRAINID=A
+TRAINID=$1
 printf "===== Evaluating setup $TRAINID =====\n"
 
-CORPUS=test.swc.old
-C=test-old
+CORPUS=$2
+C=$3
 SRC=swc
 TGT=fra
 printf "TEST SET: $CORPUS\n"
 
 MODELTAG=generic-s001
-printf "MODEL: $TRAINID-$MODELTAG\n"
+MODELSRC=sw #swc for Procedure A
+MODELTGT=fr #fra for Procedure A
+printf "\nMODEL: $TRAINID-$MODELTAG\n"
 
 printf "$SRC->$TGT\n"
-bash 8a-inference.sh $TRAINID $MODELTAG $BPEID $CORPUS $C $SRC $TGT
-bash 8b-evaluate.sh $TRAINID $MODELTAG $CORPUS $C $SRC $TGT
+bash 8a-inference.sh $TRAINID $MODELSRC-$MODELTGT-$MODELTAG $BPEID $CORPUS $C $SRC $TGT
+bash 8b-evaluate.sh $TRAINID $MODELSRC-$MODELTGT-$MODELTAG $CORPUS $C $SRC $TGT
 
 printf "\n$TGT->$SRC\n"
-bash 8a-inference.sh $TRAINID $MODELTAG $BPEID $CORPUS $C $TGT $SRC
-bash 8b-evaluate.sh $TRAINID $MODELTAG $CORPUS $C $TGT $SRC
+bash 8a-inference.sh $TRAINID $MODELTGT-$MODELSRC-$MODELTAG $BPEID $CORPUS $C $TGT $SRC
+bash 8b-evaluate.sh $TRAINID $MODELTGT-$MODELSRC-$MODELTAG $CORPUS $C $TGT $SRC
+
+MODELTAG=inswc-s001-i001
+MODELSRC=swc
+MODELTGT=fra
+printf "\nMODEL: $TRAINID-$MODELTAG\n"
+
+printf "$SRC->$TGT\n"
+bash 8a-inference.sh $TRAINID $MODELSRC-$MODELTGT-$MODELTAG $BPEID $CORPUS $C $SRC $TGT
+bash 8b-evaluate.sh $TRAINID $MODELSRC-$MODELTGT-$MODELTAG $CORPUS $C $SRC $TGT
+
+printf "\n$TGT->$SRC\n"
+bash 8a-inference.sh $TRAINID $MODELTGT-$MODELSRC-$MODELTAG $BPEID $CORPUS $C $TGT $SRC
+bash 8b-evaluate.sh $TRAINID $MODELTGT-$MODELSRC-$MODELTAG $CORPUS $C $TGT $SRC
+
+#Comment out from here for Procedure A
+MODELTAG=intwb-s001-i001-t001
+MODELSRC=swc
+MODELTGT=fra
+printf "\nMODEL: $TRAINID-$MODELTAG\n"
+
+printf "$SRC->$TGT\n"
+bash 8a-inference.sh $TRAINID $MODELSRC-$MODELTGT-$MODELTAG $BPEID $CORPUS $C $SRC $TGT
+bash 8b-evaluate.sh $TRAINID $MODELSRC-$MODELTGT-$MODELTAG $CORPUS $C $SRC $TGT
+
+printf "\n$TGT->$SRC\n"
+bash 8a-inference.sh $TRAINID $MODELTGT-$MODELSRC-$MODELTAG $BPEID $CORPUS $C $TGT $SRC
+bash 8b-evaluate.sh $TRAINID $MODELTGT-$MODELSRC-$MODELTAG $CORPUS $C $TGT $SRC
+
+
+#alert 
+echo -en "\007"
